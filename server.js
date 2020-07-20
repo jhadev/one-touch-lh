e8721cae74b046f8912d9dc1f25e970439fd7036
jhadev <joshappeldev@gmail.com>
2020-07-20 10:25:23 -0400

    heroku

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const serveIndex = require('serve-index');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
